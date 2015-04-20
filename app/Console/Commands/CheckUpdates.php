bb8a85edffa1bc223b702397e83f4e3c53f618cf
Yury Kozyrev <yury.kozyrev@home24.de>
2015-04-21 00:17:38 +0200

    design + fix tests + cron command

---
+++ app/Console/Commands/CheckUpdates.php
@@ -0,0 +1,78 @@
+<?php namespace App\Console\Commands;
+
+use App\Components\Retriever;
+use App\Components\StorageAdapter;
+use Illuminate\Console\Command;
+use Kozz\Laravel\Facades\Guzzle;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class CheckUpdates extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
+
+    protected $retriever;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->retriever = new Retriever(Guzzle::getFacadeRoot(), new StorageAdapter(\Cache::getFacadeRoot()));
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $this->retriever->retrieve()->save();
+        $results = count($this->retriever->getResults());
+        $test    = date("Y-m-d H:i:s") . " - Got " . $results . " results\n";
+        $this->comment($test);
+        file_put_contents("tmp.log", $test, FILE_APPEND);
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+//			['example', InputArgument::REQUIRED, 'An example argument.'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+//			['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+        ];
+    }
+
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -44,12 +44,12 @@ class ExampleTest extends TestCase
         $response = $this->call('GET', '/asd');
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals("Empty id", $response->getContent());
+        $this->assertContains("Invalid number", $response->getContent());
 
         $response = $this->call('GET', '/000');
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals("Empty id", $response->getContent());
+        $this->assertContains("Invalid number", $response->getContent());
     }
 
     /**
@@ -67,36 +67,9 @@ class ExampleTest extends TestCase
 
         $response = $this->call('GET', '/123');
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('123 Pass 30.04.15', $response->getContent());
+        $this->assertContains('123 Pass 30.04.15', $response->getContent());
     }
 
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testCheckGetCall()
-    {
-        $this->cache->expects($this->any())->method('__call')->willReturnCallback(function ($method, array $args) {
-            if ('forever' === $method) {
-                return;
-            }
-            if ('get' === $method) {
-                return;
-            }
-        });
-
-        $stream = new \GuzzleHttp\Stream\BufferStream();
-        $stream->write(file_get_contents(__DIR__ . '/fixtures/fixture.pdf'));
-
-        app('guzzle')->shouldReceive('get')->andReturn(new GuzzleHttp\Message\Response(200, ['X-Foo' => 'Bar'], $stream));
-
-        $response = $this->call('GET', '/2953799');
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('2953799 (3086682)  RP, WS, KV  26.06.2015 ', $response->getContent());
-
-    }
 
     /**
      * A basic functional test example.
@@ -122,7 +95,7 @@ class ExampleTest extends TestCase
         $response = $this->call('GET', '/1000000');
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('Id not found', $response->getContent());
+        $this->assertContains('Your visa result not found', $response->getContent());
 
     }
 
