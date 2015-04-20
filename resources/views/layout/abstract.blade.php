0237a245b35d98a58ec68e50f493af0ddba5f235
Yury Kozyrev <yury.kozyrev@home24.de>
2015-04-20 21:52:40 +0200

    blocker fix

--- resources/views/layout/abstract.blade.php
+++ resources/views/layout/abstract.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>InstaTranslate @yield('title')</title>
+    <title>Visa Check @yield('title')</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
