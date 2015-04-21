a01f1483336d5b6a3ebc151e1f4df3566209778c
Yury Kozyrev <yury.kozyrev@home24.de>
2015-04-21 18:21:31 +0200

    layout fixes

--- resources/views/layout/abstract.blade.php
+++ resources/views/layout/abstract.blade.php
@@ -7,7 +7,7 @@
 <link href="/assets/css/app.css" rel="stylesheet">
 <link href='//fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
 <script async src="/assets/js/app.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
